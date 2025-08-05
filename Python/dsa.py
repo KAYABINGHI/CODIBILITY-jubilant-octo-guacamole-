a26c6d063cc9843465ff78a8ea2c22474e957030
KAYABINGHI <muchaipauls@gmail.com>
2025-08-05 11:47:12 +0300

    exact

--- Python/dsa.py
+++ Python/dsa.py
@@ -2,7 +2,7 @@
 
 def missing_number(nums):
     n = len(nums)
-    expected_sum = n * (n + 1) // 2  # Sum of numbers from 0 to n
+    expected_sum = n * (n + 1) / 2  # Sum of numbers from 0 to n
     actual_sum = sum(nums)
     return expected_sum - actual_sum
 
